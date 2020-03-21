de88beb5e43f5321fb65e5572db0001b8f8074a8
Christian Valencia <christianvalenciav@gmail.com>
2020-03-21 13:32:59 -0400

    Initial Commit

---
+++ King of FIFA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  King of FIFA
+//
+//  Created by Christian Valencia on 3/21/20.
+//  Copyright © 2020 Christian Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
